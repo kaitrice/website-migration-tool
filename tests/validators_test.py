a7f0dec9ca2ca52e3a9ec4bdf2776c4c88889127
Kaitlyn Rice <kaitnrice@gmail.com>
2025-12-03 01:33:38 -0800

    fix lint errors

--- tests/validators_test.py
+++ tests/validators_test.py
@@ -1,7 +1,13 @@
+"""
+Test validator functions
+"""
 from tool.validators import is_url
 
 
 def test_is_url():
-    assert is_url("") == False
-    assert is_url("domain.com") == False
-    assert is_url("https://domain.com") == True
+    """
+    Test "is url" function
+    """
+    assert is_url('') is not False
+    assert is_url('domain.com') is not False
+    assert is_url('https://domain.com') is True
